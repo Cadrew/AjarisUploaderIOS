ab68f04fdeba7cc6faa7e658c916d0765772096d
Adrien CANINO <adrien.canino@mistale.com>
2020-03-25 18:59:49 +0000

    Begin about screen

--- AjarisUploader/AboutView.swift
+++ AjarisUploader/AboutView.swift
@@ -10,7 +10,34 @@ import SwiftUI
 
 struct AboutView: View {
     var body: some View {
-        Text("About")
+        VStack {
+            Spacer()
+            
+            Text("Ajaris UpLoader version 1.0.0")
+            
+            Text("Ajaris est un produit orkis.com")
+            
+            Spacer()
+            
+            Button(action: rateApp) {
+                Text("Noter l'application".uppercased())
+                    .frame(minWidth: 0, maxWidth: 220, minHeight: 0, maxHeight: 50, alignment: .center)
+                    .foregroundColor(Color.white)
+                    .background(Color(red: 51 / 255, green: 108 / 255, blue: 202 / 255))
+                    .cornerRadius(5)
+                    .disabled(true)
+            }
+            
+            Spacer()
+        }
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+        .background(
+        Image("ajaris_background")
+            .resizable())
+    }
+    
+    private func rateApp() {
+        // TODO
     }
 }
 
